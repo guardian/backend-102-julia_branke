43d105a3ccfd1b0413784f92d2a0b1f0e36b253c
Julia Branke <Julia.Branke@guardian.co.uk>
2022-08-03 17:33:27 +0100

    Updated ci.yaml

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -14,7 +14,7 @@ object Hello {
         def handle(httpExchange: HttpExchange): Unit = {
           println(s"GET ${httpExchange.getRequestURI().getPath()} received.")
 
-          val response = "Hello, World!"
+          val response = "Hello, Backend 102!"
           httpExchange.sendResponseHeaders(200, response.length)
           val out = httpExchange.getResponseBody()
 
